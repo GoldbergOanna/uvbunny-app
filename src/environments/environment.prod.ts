209876a1c4c70e2fddeea18cf1c891a47f441d5f
Oana Goldberg <goldbergoanna@gmail.com>
2025-08-03 01:24:40 +0300

    🔧 add safe placeholder environment.ts for Angular build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'REPLACED_IN_CI',
+    authDomain: 'REPLACED_IN_CI',
+    projectId: 'REPLACED_IN_CI',
+    storageBucket: 'REPLACED_IN_CI',
+    messagingSenderId: 'REPLACED_IN_CI',
+    appId: 'REPLACED_IN_CI',
+    measurementId: 'REPLACED_IN_CI'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'REPLACED_IN_CI',
+    authDomain: 'REPLACED_IN_CI',
+    projectId: 'REPLACED_IN_CI',
+    storageBucket: 'REPLACED_IN_CI',
+    messagingSenderId: 'REPLACED_IN_CI',
+    appId: 'REPLACED_IN_CI',
+    measurementId: 'REPLACED_IN_CI'
+  }
+};
+
+// FOR LOCAL DEV:
+// uncomment the line below and comment out the `export const` above.
+
+// export * from './environment.local';
