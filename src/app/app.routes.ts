131f8103d3aa8c68b964ec92c7a0e384c07c2c11
Oana Goldberg <goldbergoanna@gmail.com>
2025-08-02 17:04:33 +0300

    feat(config): Add config page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { BunnyDetailsComponent } from './pages/bunny-details/bunny-details.component';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
     title: 'Dashboard - UVbunny'
+  },
+  {
+    path: 'configuration',
+    component: ConfigurationComponent,
+    title: 'Configuration - UVbunny'
   },
   {
     path: 'bunny/:id',
